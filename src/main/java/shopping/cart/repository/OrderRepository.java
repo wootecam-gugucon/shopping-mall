e585872b504eed525071518508e1354d9b1ddcce
gunner6603 <gunner6603@snu.ac.kr>
2023-08-07 23:53:55 +0900

    feat: OrderRepository 작성

--- src/main/java/shopping/cart/repository/OrderRepository.java
+++ src/main/java/shopping/cart/repository/OrderRepository.java
@@ -2,9 +2,9 @@ package shopping.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import shopping.cart.domain.entity.Product;
+import shopping.cart.domain.entity.Order;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
