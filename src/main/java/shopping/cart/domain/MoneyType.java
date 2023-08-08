8a55c9b79ca86e97659764a0dd6ae21d79ef6cec
gunner6603 <gunner6603@snu.ac.kr>
2023-08-08 21:06:18 +0900

    feat: MoneyType 작성

---
+++ src/main/java/shopping/cart/domain/MoneyType.java
@@ -0,0 +1,14 @@
+package shopping.cart.domain;
+
+public enum MoneyType {
+
+    USD("달러"),
+    KRW("원"),
+    ;
+
+    private final String unit;
+
+    MoneyType(final String unit) {
+        this.unit = unit;
+    }
+}
