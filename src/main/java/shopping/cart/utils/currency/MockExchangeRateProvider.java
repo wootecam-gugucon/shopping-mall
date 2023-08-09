6c4b066989b6c57d62e83a1d879790cdd20b0749
gunner6603 <gunner6603@snu.ac.kr>
2023-08-09 15:43:46 +0900

    refactor: mock 사용하지 않도록 변경

--- src/main/java/shopping/cart/utils/currency/MockExchangeRateProvider.java
+++ src/main/java/shopping/cart/utils/currency/MockExchangeRateProvider.java
@@ -1,12 +1,10 @@
 package shopping.cart.utils.currency;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
 import shopping.cart.domain.MoneyType;
 import shopping.cart.domain.vo.ExchangeRate;
 
-@Component
-@Primary
+//@Component
+//@Primary
 public class MockExchangeRateProvider implements ExchangeRateProvider {
 
     @Override
