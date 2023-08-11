4650e104ed24af3300a8046572f7610c98fc5196
gunner6603 <gunner6603@snu.ac.kr>
2023-08-11 16:09:45 +0900

    refactor: RestTemplate 설정 분리

--- src/main/java/shopping/cart/config/ExchangeRateProviderConfig.java
+++ src/main/java/shopping/cart/config/ExchangeRateProviderConfig.java
@@ -1,7 +1,5 @@
 package shopping.cart.config;
 
-import java.time.Duration;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,17 +10,15 @@ import shopping.cart.utils.currency.ExchangeRateProvider;
 @Configuration
 public class ExchangeRateProviderConfig {
 
-    @Bean
-    public RestTemplate customRestTemplate() {
-        return new RestTemplateBuilder()
-            .setConnectTimeout(Duration.ofSeconds(5))
-            .setReadTimeout(Duration.ofSeconds(5))
-            .build();
+    private final RestTemplate restTemplate;
+
+    public ExchangeRateProviderConfig(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     @Bean
     @Profile("!test")
     public ExchangeRateProvider defaultExchangeRateProvider() {
-        return new DefaultExchangeRateProvider(customRestTemplate());
+        return new DefaultExchangeRateProvider(restTemplate);
     }
 }
