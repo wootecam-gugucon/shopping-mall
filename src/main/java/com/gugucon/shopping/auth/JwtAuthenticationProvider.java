517db00b3989ede9c22ee75e51754ea4afcb7754
rimrim990 <lim990718@naver.com>
2023-08-14 08:12:34 +0900

    refactor: jwt 토큰 검증 로직 추가

--- src/main/java/com/gugucon/shopping/auth/JwtAuthenticationProvider.java
+++ src/main/java/com/gugucon/shopping/auth/JwtAuthenticationProvider.java
@@ -1,9 +1,11 @@
 package com.gugucon.shopping.auth;
 
+import com.gugucon.shopping.common.exception.ErrorCode;
 import com.gugucon.shopping.member.utils.JwtProvider;
 import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
@@ -17,6 +19,7 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String jwtToken = ((JwtAuthenticationToken) authentication).getJwtToken();
+        validateToken(jwtToken);
         String principal = jwtProvider.parseToken(jwtToken);
         return new JwtAuthenticationToken(principal, "", new ArrayList<>());
     }
@@ -24,5 +27,11 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
     @Override
     public boolean supports(Class<?> authentication) {
         return JwtAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+    private void validateToken(String jwtToken) {
+        if (!jwtProvider.validate(jwtToken)) {
+            throw new BadCredentialsException(ErrorCode.INVALID_TOKEN.getMessage());
+        }
     }
 }
