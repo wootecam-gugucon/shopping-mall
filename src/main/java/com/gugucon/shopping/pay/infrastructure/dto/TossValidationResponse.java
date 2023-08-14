a2c92d5f76a9f7d42f97514ef8c1bce7e70500ee
dongjoo0-0 <kdj486347@gmail.com>
2023-08-14 15:42:35 +0900

    refactor: JsonCreator 삭제

--- src/main/java/com/gugucon/shopping/pay/infrastructure/dto/TossValidationResponse.java
+++ src/main/java/com/gugucon/shopping/pay/infrastructure/dto/TossValidationResponse.java
@@ -1,28 +1,17 @@
 package com.gugucon.shopping.pay.infrastructure.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public final class TossValidationResponse {
 
-    private final String paymentKey;
-    private final String status;
-    private final String orderId;
-    private final String orderName;
-    private final String method;
-
-    @JsonCreator
-    public TossValidationResponse(@JsonProperty("paymentKey") final String paymentKey,
-                                  @JsonProperty("status") final String status,
-                                  @JsonProperty("orderId") final String orderId,
-                                  @JsonProperty("orderName") final String orderName,
-                                  @JsonProperty("method") final String method) {
-        this.paymentKey = paymentKey;
-        this.status = status;
-        this.orderId = orderId;
-        this.orderName = orderName;
-        this.method = method;
-    }
+    private String paymentKey;
+    private String status;
+    private String orderId;
+    private String orderName;
+    private String method;
 }
