2bb2fd6101ed6ee36be27e3919194853863925a4
rimrim990 <lim990718@naver.com>
2023-08-16 02:12:28 +0900

    fix: JwtAuthentication equals, hashCode, toString 구현

--- src/main/java/com/gugucon/shopping/auth/domain/vo/JwtAuthenticationToken.java
+++ src/main/java/com/gugucon/shopping/auth/domain/vo/JwtAuthenticationToken.java
@@ -1,6 +1,7 @@
 package com.gugucon.shopping.auth.domain.vo;
 
 import java.util.Collection;
+import java.util.Objects;
 import lombok.Getter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
@@ -27,5 +28,35 @@ public class JwtAuthenticationToken extends AbstractAuthenticationToken {
         this.principal = principal;
         this.credentials = credentials;
         super.setAuthenticated(true);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        final JwtAuthenticationToken that = (JwtAuthenticationToken) o;
+        return Objects.equals(jwtToken, that.jwtToken) && Objects.equals(principal, that.principal)
+            && Objects.equals(credentials, that.credentials);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), jwtToken, principal, credentials);
+    }
+
+    @Override
+    public String toString() {
+        return "JwtAuthenticationToken{" +
+            "jwtToken='" + jwtToken + '\'' +
+            ", principal=" + principal +
+            ", credentials=" + credentials +
+            '}';
     }
 }
