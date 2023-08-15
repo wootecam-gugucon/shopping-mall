1b100a4e425545c4aace3cc0b95cde43c575bbc5
gunner6603 <gunner6603@snu.ac.kr>
2023-08-15 20:05:44 +0900

    refactor: Validated -> Valid 변경

--- src/main/java/com/gugucon/shopping/item/controller/api/CartItemController.java
+++ src/main/java/com/gugucon/shopping/item/controller/api/CartItemController.java
@@ -5,9 +5,9 @@ import com.gugucon.shopping.item.dto.request.CartItemUpdateRequest;
 import com.gugucon.shopping.item.dto.response.CartItemResponse;
 import com.gugucon.shopping.item.service.CartService;
 import com.gugucon.shopping.member.argumentresolver.annotation.MemberId;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +21,7 @@ public class CartItemController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public void insertCartItem(@RequestBody @Validated final CartItemInsertRequest cartItemInsertRequest,
+    public void insertCartItem(@RequestBody @Valid final CartItemInsertRequest cartItemInsertRequest,
                                @MemberId final Long memberId) {
         cartService.insertCartItem(cartItemInsertRequest, memberId);
     }
@@ -35,7 +35,7 @@ public class CartItemController {
     @PutMapping("/{cartItemId}/quantity")
     @ResponseStatus(HttpStatus.OK)
     public void updateCartItemQuantity(@PathVariable final Long cartItemId,
-                                       @RequestBody @Validated final CartItemUpdateRequest cartItemUpdateRequest,
+                                       @RequestBody @Valid final CartItemUpdateRequest cartItemUpdateRequest,
                                        @MemberId final Long memberId) {
         cartService.updateCartItemQuantity(cartItemId, cartItemUpdateRequest, memberId);
     }
