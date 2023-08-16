9423f108284cf0499c935daab6e213648b475ebc
dahyen0o <seeunchoi99@gmail.com>
2023-08-16 17:28:23 +0900

    feat: 프론트에서 물품의 재고가 0일 때 장바구니 추가 버튼이 disabled 되는 기능 구현

--- src/main/java/com/gugucon/shopping/item/domain/vo/Stock.java
+++ src/main/java/com/gugucon/shopping/item/domain/vo/Stock.java
@@ -5,9 +5,11 @@ import com.gugucon.shopping.common.exception.ShoppingException;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Stock {
