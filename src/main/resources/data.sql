602e43f16c38c44a49f25ae0e5ab84f721d541f5
dahyen0o <seeunchoi99@gmail.com>
2023-08-16 21:44:11 +0900

    fix: 빌드 에러 해결 및 gitignore에 application.properties 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ values (2, '피자', 'pizza.png', 100, 'test_description', 25000, now(), now());
 insert into products(id, name, image_file_name, stock, description, price, created_at, last_modified_at)
 values (3, '사케', 'sake.png', 100, 'test_description', 30000, now(), now());
 insert into products(id, name, image_file_name, stock, description, price, created_at, last_modified_at)
-values (4, '품절된 치킨', 'fried_chicken.png', 0, 'test_description', 20000, now(), now());
+values (4, '품절된 치킨', 'null.png', 0, 'test_description', 20000, now(), now());
 
 insert into members(id, email, password, nickname, created_at, last_modified_at)
 values (1, 'test_email@woowafriends.com', 'test_password!', 'tester1', now(), now());
