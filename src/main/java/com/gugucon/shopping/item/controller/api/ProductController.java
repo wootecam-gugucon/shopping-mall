9ffff30a9735e8629f48723225482f6557f52d63
rimrim990 <lim990718@naver.com>
2023-08-17 16:03:31 +0900

    feat: 기본 정렬 설정

--- src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
+++ src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
@@ -5,6 +5,8 @@ import com.gugucon.shopping.item.dto.response.ProductResponse;
 import com.gugucon.shopping.item.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,8 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public PagedResponse<ProductResponse> getProducts(final Pageable pageable) {
+    public PagedResponse<ProductResponse> getProducts(@SortDefault(sort = "createdAt", direction = Direction.DESC)
+                                                          final Pageable pageable) {
         return productService.readAllProducts(pageable);
     }
 }
