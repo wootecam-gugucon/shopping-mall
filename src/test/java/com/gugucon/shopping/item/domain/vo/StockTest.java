82c17642f6f0b01d0dcfe6ae3644f0a7a3484cd4
dahyen0o <seeunchoi99@gmail.com>
2023-08-17 11:21:40 +0900

    test: StockTest '요청 수량과 재고 비교' 테스트

--- src/test/java/com/gugucon/shopping/item/domain/vo/StockTest.java
+++ src/test/java/com/gugucon/shopping/item/domain/vo/StockTest.java
@@ -37,7 +37,7 @@ class StockTest {
 
     @Test
     @DisplayName("재고가 0일 시 품절 여부를 true로 반환한다.")
-    void isSoldOutTrue_valueIsZero() {
+    void isSoldOut() {
         // given
         final Stock stock = Stock.from(0);
 
@@ -51,7 +51,7 @@ class StockTest {
     @ParameterizedTest
     @ValueSource(ints = {1, Integer.MAX_VALUE})
     @DisplayName("재고가 0이 아닐 시 품절 여부를 false로 반환한다.")
-    void isSoldOutTrue_valueIsNotZero(final int value) {
+    void isSoldOut_valueIsNotZero(final int value) {
         // given
         final Stock stock = Stock.from(value);
 
@@ -61,4 +61,31 @@ class StockTest {
         // then
         assertThat(result).isFalse();
     }
-}
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5})
+    @DisplayName("요청 수량보다 재고가 같거나 많으면 true를 반환하다.")
+    void canBuy(final int quantity) {
+        // given
+        final Stock stock = Stock.from(5);
+
+        // when
+        final boolean result = stock.canBuy(quantity);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {6, Integer.MAX_VALUE})
+    @DisplayName("요청 수량보다 재고가 적으면 false를 반환한다.")
+    void canBuy_manyQuantity(final int quantity) {
+        // given
+        final Stock stock = Stock.from(5);
+
+        // when
+        final boolean result = stock.canBuy(quantity);
+
+        // then
+        assertThat(result).isFalse();
+    }}
