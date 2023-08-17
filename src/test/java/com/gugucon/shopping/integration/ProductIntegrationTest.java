50ccab5d2cb96c68b321670293e69ee109114cb9
rimrim990 <lim990718@naver.com>
2023-08-17 15:04:58 +0900

    test: 상품 기본 정렬 인수 테스트

--- src/test/java/com/gugucon/shopping/integration/ProductIntegrationTest.java
+++ src/test/java/com/gugucon/shopping/integration/ProductIntegrationTest.java
@@ -7,13 +7,12 @@ import com.gugucon.shopping.common.dto.response.PagedResponse;
 import com.gugucon.shopping.item.dto.response.ProductResponse;
 import com.gugucon.shopping.member.dto.request.LoginRequest;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.Comparator;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 
 @DisplayName("상품 기능 통합 테스트")
@@ -39,17 +38,14 @@ class ProductIntegrationTest extends IntegrationTest {
     }
 
     @Test
-    @DisplayName("상품 전체 목록을 페이징하여 조회한다.")
-    void readAllProducts_paging() {
+    @DisplayName("페이징 조건이 기재되지 않으면 기본 설정에 따라 페이징하여 반환한다.")
+    void readAllProducts_defaultPaging() {
         /* given */
         String accessToken = login(new LoginRequest("test_email@woowafriends.com", "test_password!"));
-        PageRequest pageRequest = PageRequest.of(0, 20);
 
         /* when */
         final ExtractableResponse<Response> response = RestAssured
             .given().log().all()
-            .body(pageRequest)
-            .contentType(ContentType.JSON)
             .auth().oauth2(accessToken)
             .when().get("/api/v1/product")
             .then().log().all()
@@ -64,5 +60,55 @@ class ProductIntegrationTest extends IntegrationTest {
 
         assertThat(products).hasSize(3);
         assertThat(lastPage).isTrue();
+    }
+
+    @Test
+    @DisplayName("입력된 페이징 조건에 따라 페이징하여 반환한다.")
+    void readAllProducts_paging() {
+        /* given */
+        String accessToken = login(new LoginRequest("test_email@woowafriends.com", "test_password!"));
+
+        /* when */
+        final ExtractableResponse<Response> response = RestAssured
+            .given().log().all()
+            .auth().oauth2(accessToken)
+            .when().get("/api/v1/product?page=0&size=1")
+            .then().log().all()
+            .extract();
+
+        /* then */
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        final PagedResponse result = response.as(PagedResponse.class);
+        final List<ProductResponse> products = result.getData();
+        final boolean lastPage = result.isLastPage();
+
+        assertThat(products).hasSize(1);
+        assertThat(lastPage).isFalse();
+    }
+
+    @Test
+    @DisplayName("정렬 조건이 기재되지 않은 경우 최신순으로 정렬하여 반환한다")
+    void readAllProducts_defaultSorting() {
+        /* given */
+        String accessToken = login(new LoginRequest("test_email@woowafriends.com", "test_password!"));
+
+        /* when */
+        final ExtractableResponse<Response> response = RestAssured
+            .given().log().all()
+            .auth().oauth2(accessToken)
+            .when().get("/api/v1/product")
+            .then().log().all()
+            .extract();
+
+        /* then */
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        final List<ProductResponse> products = response.body().jsonPath().getList("$.data", ProductResponse.class);
+        final List<ProductResponse> sortedProducts = products.stream()
+            .sorted(Comparator.comparing(ProductResponse::getId).reversed())
+            .toList();
+
+        assertThat(products).containsExactlyElementsOf(sortedProducts);
     }
 }
