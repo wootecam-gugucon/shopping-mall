2ee35687b8d11b9558be9705371e225ce666448b
rimrim990 <lim990718@naver.com>
2023-08-18 10:30:29 +0900

    test: 테스트 환경 분리

--- src/test/java/com/gugucon/shopping/integration/config/IntegrationTest.java
+++ src/test/java/com/gugucon/shopping/integration/config/IntegrationTest.java
@@ -1,11 +1,12 @@
 package com.gugucon.shopping.integration.config;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Retention(RetentionPolicy.RUNTIME)
 @TestExecutionListeners(value = {IntegrationTestExecutionListener.class,}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
--- src/test/java/com/gugucon/shopping/pay/integration/IntegrationTest.java
+++ src/test/java/com/gugucon/shopping/pay/integration/IntegrationTest.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
