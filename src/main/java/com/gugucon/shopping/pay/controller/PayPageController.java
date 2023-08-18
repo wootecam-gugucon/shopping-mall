b5f475498a8412f8281db8108cc2e949a5c8a4ff
gunner6603 <gunner6603@snu.ac.kr>
2023-08-18 16:36:29 +0900

    refactor: RequestMapping 추가

--- src/main/java/com/gugucon/shopping/pay/controller/PayPageController.java
+++ src/main/java/com/gugucon/shopping/pay/controller/PayPageController.java
@@ -6,28 +6,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/pay")
 public class PayPageController {
 
 
-    @GetMapping("/pay/{payId}")
+    @GetMapping("/{payId}")
     public String payPage(@PathVariable final Long payId) {
         return "pay";
     }
 
 
-    @GetMapping("/pay/loading-popup")
+    @GetMapping("/loading-popup")
     public String getLoadingPagePopUp(@ModelAttribute final PayValidationRequest payValidationRequest) {
         return "pay-loading-popup";
     }
 
-    @GetMapping("/pay/fail-popup")
+    @GetMapping("/fail-popup")
     public String getFailPagePopUp(@ModelAttribute final PayFailParameter payFailParameter) {
         return "pay-fail-popup";
     }
 
-    @GetMapping("/pay/success")
+    @GetMapping("/success")
     public String getSuccessPage() {
         return "pay-success";
     }
