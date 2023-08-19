99d3b53cb8835b156fdf03c349f51d0d72ff1e8c
dahyen0o <seeunchoi99@gmail.com>
2023-08-19 12:06:11 +0900

    test: ProductIntegrationTest '검색어로 검색한 결과를 최신순으로 정렬하여 반환한다' 테스트

--- src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
+++ src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.SortDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +21,7 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public PagedResponse<ProductResponse> getProducts(@SortDefault(sort = "createdAt", direction = Direction.DESC)
                                                           final Pageable pageable) {
         return productService.readAllProducts(pageable);
