a2aac1655723c53b81978a82c877a7875a211213
rimrim990 <lim990718@naver.com>
2023-08-19 20:54:08 +0900

    delete: data.sql 제거

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into products(id, name, image_file_name, stock, description, price, created_at, last_modified_at)
-values (default, '치킨', 'fried_chicken.png', 100, 'test_description', 20000, now(), now());
-insert into products(id, name, image_file_name, stock, description, price, created_at, last_modified_at)
-values (default, '피자', 'pizza.png', 100, 'test_description', 25000, now(), now());
-insert into products(id, name, image_file_name, stock, description, price, created_at, last_modified_at)
-values (default, '사케', 'sake.png', 100, 'test_description', 30000, now(), now());
-insert into products(id, name, image_file_name, stock, description, price, created_at, last_modified_at)
-values (default, '품절된 치킨', 'null.png', 0, 'test_description', 20000, now(), now());
