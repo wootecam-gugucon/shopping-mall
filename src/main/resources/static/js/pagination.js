37f72d4083317103c472306618b4570de58d93d2
dongjoo <46925347+dongjoo0-0@users.noreply.github.com>
2023-08-19 21:49:57 +0900

    feat: 상품 조회 페이징 프론트에 반영 (#51)
    * feat: 상품 조회 페이징 프론트에 반영
    
    * fix: EOL 설정
    
    * fix: 마지막 페이지 볼 수 있도록 조건 수정

---
+++ src/main/resources/static/js/pagination.js
@@ -0,0 +1,33 @@
+const pagination = (currentPage, totalPage, size, className) => {
+
+  if (totalPage > 1) {
+    const pageCount = 5;
+    const pageGroup = Math.ceil((currentPage + 1) / pageCount);
+    const last = (pageCount * pageGroup) > totalPage ? totalPage : pageCount
+        * pageGroup;
+    const first = pageCount * (pageGroup - 1) + 1;
+
+    let pages = '';
+    pages += `
+          <button id="page-${first - 1}" onclick="location.href='/?page=${first - 2}&size=${size}'">이전</button>
+        `;
+    for (let i = first; i <= last; i++) {
+      pages += `
+            <button id="page-${i}" onclick="location.href='/?page=${i - 1}&size=${size}'">${i}</button>
+          `;
+    }
+    pages += `
+          <button id="page-${last + 1}" onclick="location.href='/?page=${last}&size=${size}'">다음</button>
+        `;
+    document.querySelector(className).innerHTML = pages;
+    document.getElementById(`page-${currentPage + 1}`).classList.add("active");
+    if (last >= totalPage) {
+      const next = document.getElementById(`page-${last + 1}`);
+      next.setAttribute("disabled", true);
+    }
+    if (first - 1 <= 0) {
+      const prev = document.getElementById(`page-${first - 1}`);
+      prev.setAttribute("disabled", true);
+    }
+  }
+}
