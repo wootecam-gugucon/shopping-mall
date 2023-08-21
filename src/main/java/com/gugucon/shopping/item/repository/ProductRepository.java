d423be15bc36be2818ea68af85b9e24d8ec4a5ba
dahyen0o <seeunchoi99@gmail.com>
2023-08-21 21:41:18 +0900

    feat: ProductRepositoryTest '해당 키워드를 이름에 포함하는 product를 주문이 많은 순으로 조회한다' 구현

--- src/main/java/com/gugucon/shopping/item/repository/ProductRepository.java
+++ src/main/java/com/gugucon/shopping/item/repository/ProductRepository.java
@@ -5,10 +5,19 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByNameContainingIgnoreCase(final @NotNull String name, final Pageable pageable);
+
+    @Query("select p from Product p " +
+            "join OrderItem oi on p.id = oi.productId " +
+            "where p.name like %:keyword% " +
+            "group by p.id " +
+            "order by sum(oi.quantity.value) desc ")
+    Page<Product> findAllByNameSortByOrderCountDesc(@Param("keyword") final String keyword, final Pageable pageable);
 }
