562a5c9d755c304127d231a5e7215c674a773193
rimrim990 <62409503+rimrim990@users.noreply.github.com>
2023-08-21 16:07:20 +0900

    feat: 상품 상세 조회 페이지 구현 (#73)
    * refactor: 중복된 signUp 과 login 호출을 메서드로 분리
    
    * refactor: 불필요한 email, password 변수 선언을 제거하고 파라미터로 바로 전달
    
    * refactor: api 관련 테스트 메서드를 ApiUtils 로 분리
    
    * refactor: 테스트 domain 생성 메서드를 DomainUtils 로 분리
    
    * refactor: signUp 과 login 메서드를 ApiUtils 로 이동
    
    * refactor: util 메서드 적용
    
    * delete: data.sql 제거
    
    * fix: repository 사용하여 product 정보 생성하도록 수정
    
    * fix: 새로 추가된 테스트 케이스에 product repository 사용하도록 적용
    
    * refactor: 여러 개의 상품 생성하는 메서드 분리
    
    * refactor: 테스트 메서드 종료 하면 데이터 초기화를 위해 clean.sql 실행
    
    * refactor: 미사용 메서드 제거
    
    * refactor: final 추가
    
    * refactor: 중복된 Product 생성을 DomainUtils 로 이동
    
    * test: 상품 상세 조회 API 인수 테스트
    
    * feat: 상품 상세 조회 API 구현
    
    * feat: 상품 상세 조회 시 상품이 존재하지 않을 때 예외 처리
    
    * feat: 상품 상세 페이지 연동
    
    * refactor: 페이지 UI 수정
    
    * refactor: final 추가
    
    * delete: clean.sql 과 sql 어노테이션 제거
    
    * refactor: 사용되지 않는 pay 기능 제거

--- src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
+++ src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
@@ -1,6 +1,7 @@
 package com.gugucon.shopping.item.controller.api;
 
 import com.gugucon.shopping.common.dto.response.PagedResponse;
+import com.gugucon.shopping.item.dto.response.ProductDetailResponse;
 import com.gugucon.shopping.item.dto.response.ProductResponse;
 import com.gugucon.shopping.item.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -31,5 +37,11 @@ public class ProductController {
                                                                  @SortDefault(sort = "price")
                                                          }) final Pageable pageable) {
         return productService.searchProducts(keyword, pageable);
+    }
+
+    @GetMapping("/{productId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDetailResponse getProductDetail(@PathVariable final Long productId) {
+        return productService.getProductDetail(productId);
     }
 }
