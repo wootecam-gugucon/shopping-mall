7f22d84ff478263b53f231fa569fb3f6fc33a718
rimrim990 <lim990718@naver.com>
2023-08-21 11:56:09 +0900

    feat: 상품 상세 페이지 연동

--- src/main/java/com/gugucon/shopping/item/controller/page/ProductPageController.java
+++ src/main/java/com/gugucon/shopping/item/controller/page/ProductPageController.java
@@ -2,6 +2,7 @@ package com.gugucon.shopping.item.controller.page;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductPageController {
@@ -9,5 +10,10 @@ public class ProductPageController {
     @GetMapping("/")
     public String readAllProducts() {
         return "index";
+    }
+
+    @GetMapping("/product/{productId}")
+    public String productDetailPage(@PathVariable final String productId) {
+        return "product-detail";
     }
 }
