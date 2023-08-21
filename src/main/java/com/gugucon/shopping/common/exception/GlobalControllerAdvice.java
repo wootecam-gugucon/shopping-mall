1b909ea68878f48fdeeedf1496fa71c90badbd5b
건 <gunner6603@snu.ac.kr>
2023-08-21 14:25:32 +0900

    feat: 로깅 추가 (#64)
    * feat: getMessage 오버라이드
    
    * feat: stacktrace 기반으로 에러 로깅하도록 변경
    
    * feat: 외부 API 호출 시 로깅하도록 변경
    
    * refactor: 로깅 줄바꿈 삭제
    
    * feat: 로깅 설정 추가
    
    * fix: 로깅 조건 변경
    
    * refactor: 메서드명 변경
    
    * feat: HTTP 요청, 응답 로깅 추가

--- src/main/java/com/gugucon/shopping/common/exception/GlobalControllerAdvice.java
+++ src/main/java/com/gugucon/shopping/common/exception/GlobalControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.gugucon.shopping.common.exception;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -20,14 +22,18 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
+@Slf4j
 public class GlobalControllerAdvice {
 
     private final MessageSource messageSource;
 
+    @Value("${stacktrace.limit:1}")
+    private Integer stackTraceLimit;
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(final Exception exception) {
-        exception.printStackTrace();
+        logCustomStackTrace(exception);
         return ErrorResponse.from(ErrorCode.UNKNOWN_ERROR);
     }
 
@@ -46,6 +52,9 @@ public class GlobalControllerAdvice {
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleApplicationException(final ShoppingException exception) {
         final ErrorCode errorCode = exception.getErrorCode();
+        if (errorCode.getStatus().is5xxServerError()) {
+            logCustomStackTrace(exception);
+        }
         return ResponseEntity.status(errorCode.getStatus()).body(ErrorResponse.from(errorCode));
     }
 
@@ -69,5 +78,26 @@ public class GlobalControllerAdvice {
                 }).filter(Objects::nonNull)
                 .findFirst()
                 .orElse(error.getDefaultMessage());
+    }
+
+    private void logCustomStackTrace(final Exception e) {
+        final StackTraceElement[] stackTrace = e.getStackTrace();
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+                .append("resolved exception : ").append(e.getClass()).append("\n")
+                .append("message : ").append(e.getMessage()).append("\n")
+                .append("cause : ").append(e.getCause()).append("\n");
+
+        for (int i = 0; i < stackTraceLimit; i++) {
+            final String className = stackTrace[i].getClassName();
+            final String methodName = stackTrace[i].getMethodName();
+            final int lineNumber = stackTrace[i].getLineNumber();
+            stringBuilder
+                    .append("\n[").append(i).append("]\n")
+                    .append("className : ").append(className).append("\n")
+                    .append("methodName : ").append(methodName).append("\n")
+                    .append("lineNumber : ").append(lineNumber).append("\n");
+        }
+        log.error(stringBuilder.toString());
     }
 }
