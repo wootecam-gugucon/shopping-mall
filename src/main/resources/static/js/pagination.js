f6fef3ca6da8bc7cae358aef9f22489845b39507
dahyen0o <seeunchoi99@gmail.com>
2023-08-21 11:37:20 +0900

    feat: 검색 결과 페이지의 페이지네이션 구현

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -1,23 +1,30 @@
-const pagination = (currentPage, totalPage, size, className) => {
-
+const pagination = (currentPage, totalPage, size, className, queryParam) => {
     if (totalPage > 1) {
         const pageCount = 5;
         const pageGroup = Math.ceil((currentPage + 1) / pageCount);
-        const last = (pageCount * pageGroup) > totalPage ? totalPage : pageCount
-            * pageGroup;
+        const last = (pageCount * pageGroup) > totalPage ? totalPage : pageCount * pageGroup;
         const first = pageCount * (pageGroup - 1) + 1;
+        let search = "";
+        let keyword = "";
+        if(queryParam !== "") {
+            search = search.concat('/search');
+            keyword = keyword.concat('&').concat(queryParam);
+        }
 
         let pages = '';
         pages += `
-          <button id="page-${first - 1}" onclick="location.href='/?page=${first - 2}&size=${size}'">이전</button>
+          <button id="page-${first - 1}" 
+          onclick="location.href='${search}?page=${first - 2}&size=${size}${keyword}'">이전</button>
         `;
         for (let i = first; i <= last; i++) {
             pages += `
-            <button id="page-${i}" onclick="location.href='/?page=${i - 1}&size=${size}'">${i}</button>
+            <button id="page-${i}" 
+            onclick="location.href='${search}?page=${i - 1}&size=${size}${keyword}'">${i}</button>
           `;
         }
         pages += `
-          <button id="page-${last + 1}" onclick="location.href='/?page=${last}&size=${size}'">다음</button>
+          <button id="page-${last + 1}" 
+          onclick="location.href='${search}?page=${last}&size=${size}${keyword}'">다음</button>
         `;
         document.querySelector(className).innerHTML = pages;
         document.getElementById(`page-${currentPage + 1}`).classList.add("active");
--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -8,3 +8,20 @@ const search = () => {
 
     window.location.href = '/search?keyword='.concat(keyword);
 }
+
+const selectSortKey = (sortKey) => {
+    const buttons = document.getElementsByName("sort-select-button");
+    let button;
+    if (sortKey === null || sortKey === "") {
+        button = buttons[0];
+    } else if(sortKey === "price,desc") {
+        button = buttons[1];
+    } else if(sortKey === "price,asc") {
+        button = buttons[2];
+    } else {
+        button = buttons[3];
+    }
+    button.style.fontWeight = "bold";
+    button.style.color = "white";
+    button.style.backgroundColor = "darkslategray";
+}
