913c6fcb71adc8432401b1cb8d16f31998f8181e
rimrim990 <lim990718@naver.com>
2023-08-21 15:16:23 +0900

    delete: clean.sql 과 sql 어노테이션 제거

--- src/main/resources/clean.sql
+++
@@ -1,7 +0,0 @@
-delete from cart_items;
-delete from products;
-delete from pays;
-delete from order_items;
-delete from orders;
-delete from members;
-
--- src/test/java/com/gugucon/shopping/integration/config/IntegrationTest.java
+++ src/test/java/com/gugucon/shopping/integration/config/IntegrationTest.java
@@ -5,11 +5,8 @@ import java.lang.annotation.RetentionPolicy;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @ActiveProfiles("test")
-@Sql(scripts = "classpath:clean.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Retention(RetentionPolicy.RUNTIME)
 @TestExecutionListeners(value = {IntegrationTestExecutionListener.class,}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
