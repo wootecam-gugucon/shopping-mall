af7bd19249c44b53c4ba90124f2ecc6f632f8141
dongjoo0-0 <kdj486347@gmail.com>
2023-08-22 11:55:26 +0900

    refactor: 프론트 꾸미기

--- src/main/resources/static/js/point.js
+++ src/main/resources/static/js/point.js
@@ -1,8 +1,4 @@
-const form = document.getElementById('point-form');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
+const getPointBalance = (elementId) => {
   const credentials = localStorage.getItem('accessToken');
   if (!credentials) {
     alert('사용자 정보가 없습니다.');
@@ -10,30 +6,17 @@ form.addEventListener('submit', (event) => {
     return;
   }
 
-  const formData = new FormData(event.target);
-  let chargeRequest = {};
-  for (const entry of formData.entries()) {
-    const [key, value] = entry;
-    chargeRequest[key] = value;
-  }
-  console.log(chargeRequest);
-  console.log(JSON.stringify(chargeRequest));
-
-  fetch('/api/v1/point', {
-    method: 'PUT',
+  fetch(`/api/v1/point`, {
+    method: 'GET',
     headers: {
       'Authorization': `Bearer ${credentials}`,
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(chargeRequest)
-  }).then((response) => {
-    if (response.ok) {
-      alert("충전이 완료되었습니다.")
-      window.location.reload();
     }
-    else response.json().then((data) => alert(data.message));
+  }).then((response) => {
+    return response.json();
+  }).then((data) => {
+    document.getElementById(elementId).innerText = data.point + ' P';
   }).catch((error) => {
-    alert(error);
+    console.error(error);
   });
-
-});
+}
