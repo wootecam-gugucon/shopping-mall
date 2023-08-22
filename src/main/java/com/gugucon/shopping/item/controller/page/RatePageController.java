24f5c6284053c2c17f5aa683750181bfa7dfb585
rimrim990 <lim990718@naver.com>
2023-08-22 14:56:46 +0900

    feat: rate-pop 연동

---
+++ src/main/java/com/gugucon/shopping/item/controller/page/RatePageController.java
@@ -0,0 +1,16 @@
+package com.gugucon.shopping.item.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RatePageController {
+
+    @GetMapping("/rate/orderItem/{orderItemId}")
+    public String ratePopUp(@PathVariable Long orderItemId, Model model) {
+        model.addAttribute("orderItemId", orderItemId);
+        return "rate-popup";
+    }
+}
