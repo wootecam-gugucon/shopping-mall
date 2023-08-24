367e637ab6e0921f69f076e3b73f51524072b313
gunner6603 <gunner6603@snu.ac.kr>
2023-08-24 14:33:49 +0900

    feat: 리포지토리 메서드 추가

--- src/main/java/com/gugucon/shopping/order/repository/OrderRepository.java
+++ src/main/java/com/gugucon/shopping/order/repository/OrderRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,11 +16,19 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT DISTINCT o FROM Order o " +
-            "LEFT OUTER JOIN FETCH o.orderItems " +
+            "JOIN FETCH o.orderItems " +
             "WHERE o.memberId = :memberId and o.status = :status")
     List<Order> findAllByMemberIdAndStatusWithOrderItems(@Param("memberId") Long memberId,
                                                          @Param("status") OrderStatus status,
                                                          Sort sort);
 
     Optional<Order> findByIdAndMemberId(Long id, Long memberId);
+
+    @Query("SELECT DISTINCT o FROM Order o " +
+            "JOIN FETCH o.orderItems " +
+            "WHERE o.status NOT IN :statuses AND (o.lastModifiedAt BETWEEN :start AND :end)")
+    List<Order> findAllByStatusNotInAndLastModifiedAtBetweenWithOrderItems(
+            @Param("statuses") List<OrderStatus> statuses,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end);
 }
