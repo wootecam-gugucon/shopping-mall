ba629e6d2168d9680950adaba25c82db842491c1
gunner6603 <gunner6603@snu.ac.kr>
2023-08-24 19:44:02 +0900

    feat: 주문 취소 스케줄링 로직 작성

---
+++ src/main/java/com/gugucon/shopping/order/scheduler/OrderCancelService.java
@@ -0,0 +1,50 @@
+package com.gugucon.shopping.order.scheduler;
+
+import com.gugucon.shopping.order.domain.entity.Order;
+import com.gugucon.shopping.order.repository.OrderRepository;
+import com.gugucon.shopping.order.service.OrderService;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.gugucon.shopping.order.domain.entity.Order.OrderStatus.CANCELLED;
+import static com.gugucon.shopping.order.domain.entity.Order.OrderStatus.COMPLETED;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderCancelService {
+
+    private static final Duration CANCEL_INTERVAL = Duration.ofMinutes(30);
+    private static final LocalDateTime DEFAULT_SCAN_START_TIME = LocalDateTime.of(2023, 1, 1, 0, 0);
+
+    private final OrderService orderService;
+    private final OrderRepository orderRepository;
+    private final EntityManager em;
+    private LocalDateTime lastScanTime;
+
+    @Transactional
+    public void cancelIncompleteOrders() {
+        log.info("cancelling started");
+        final LocalDateTime scanStartTime = lastScanTime == null ? DEFAULT_SCAN_START_TIME : lastScanTime;
+        final LocalDateTime scanEndTime = LocalDateTime.now().minus(CANCEL_INTERVAL);
+        if (scanStartTime.isAfter(scanEndTime)) {
+            log.warn("scan start time is after scan end time. schedule is cancelled.");
+            return;
+        }
+        final List<Order> incompleteOrders = orderRepository.findAllByStatusNotInAndLastModifiedAtBetweenWithOrderItems(
+                List.of(CANCELLED, COMPLETED),
+                scanStartTime,
+                scanEndTime);
+        log.info("number of incomplete orders={}", incompleteOrders.size());
+        incompleteOrders.forEach(orderService::cancelOrder);
+        lastScanTime = scanEndTime;
+        log.info("cancelling ended");
+    }
+}
