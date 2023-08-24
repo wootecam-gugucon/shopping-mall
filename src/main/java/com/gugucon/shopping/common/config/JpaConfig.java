f6fc2f36e9d49c4a17f9272748af866ccb891044
gunner6603 <gunner6603@snu.ac.kr>
2023-08-24 19:46:05 +0900

    chore: 테스트를 위한 설정 추가

--- src/main/java/com/gugucon/shopping/common/config/JpaConfig.java
+++ src/main/java/com/gugucon/shopping/common/config/JpaConfig.java
@@ -1,9 +1,11 @@
 package com.gugucon.shopping.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
+@Profile("!scheduling-test")
 public class JpaConfig {
 }
