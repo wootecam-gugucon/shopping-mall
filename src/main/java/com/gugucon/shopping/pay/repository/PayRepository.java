42e983fb83c2c43c762b581787010c0146b87d55
rimrim990 <lim990718@naver.com>
2023-08-25 17:16:41 +0900

    refactor: 사용되지 않는 메서드 제거

--- src/main/java/com/gugucon/shopping/pay/repository/PayRepository.java
+++ src/main/java/com/gugucon/shopping/pay/repository/PayRepository.java
@@ -1,12 +1,9 @@
 package com.gugucon.shopping.pay.repository;
 
 import com.gugucon.shopping.pay.domain.Pay;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PayRepository extends JpaRepository<Pay, Long> {
-
-    Optional<Pay> findByOrderId(final Long orderId);
 }
