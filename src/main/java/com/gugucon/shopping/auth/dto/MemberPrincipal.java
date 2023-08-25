2a6c614583dca46dc09d25e5e1e87c6d10dc3667
rimrim990 <lim990718@naver.com>
2023-08-25 12:16:10 +0900

    refactor: MemberPrincipal 에 member 정보 추가

--- src/main/java/com/gugucon/shopping/auth/dto/MemberPrincipal.java
+++ src/main/java/com/gugucon/shopping/auth/dto/MemberPrincipal.java
@@ -1,7 +1,9 @@
 package com.gugucon.shopping.auth.dto;
 
 import com.gugucon.shopping.member.domain.entity.Member;
+import com.gugucon.shopping.member.domain.vo.Email;
 import com.gugucon.shopping.member.domain.vo.Gender;
+import com.gugucon.shopping.member.domain.vo.Nickname;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -14,8 +16,14 @@ public class MemberPrincipal {
     private Long id;
     private LocalDate birthDate;
     private Gender gender;
+    private Email email;
+    private Nickname nickname;
 
     public static MemberPrincipal from(Member member) {
-        return new MemberPrincipal(member.getId(), member.getBirthDate(), member.getGender());
+        return new MemberPrincipal(member.getId(),
+                                   member.getBirthDate(),
+                                   member.getGender(),
+                                   member.getEmail(),
+                                   member.getNickname());
     }
 }
