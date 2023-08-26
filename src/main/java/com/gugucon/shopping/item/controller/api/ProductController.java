4327c749ef1bb58736ee0b95ced5ebcafbbae7f8
gunner6603 <gunner6603@snu.ac.kr>
2023-08-26 23:11:17 +0900

    feat: 컨트롤러 메서드 작성

--- src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
+++ src/main/java/com/gugucon/shopping/item/controller/api/ProductController.java
@@ -4,17 +4,14 @@ import com.gugucon.shopping.common.dto.response.PagedResponse;
 import com.gugucon.shopping.item.dto.response.ProductDetailResponse;
 import com.gugucon.shopping.item.dto.response.ProductResponse;
 import com.gugucon.shopping.item.service.ProductService;
+import com.gugucon.shopping.member.domain.vo.BirthYearRange;
+import com.gugucon.shopping.member.domain.vo.Gender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -25,13 +22,16 @@ public class ProductController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public PagedResponse<ProductResponse> getProducts(@SortDefault(sort = "createdAt", direction = Direction.DESC) final Pageable pageable) {
+    public PagedResponse<ProductResponse> getProducts(
+            @SortDefault(sort = "createdAt", direction = Direction.DESC) final Pageable pageable) {
         return productService.readAllProducts(pageable);
     }
 
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.OK)
     public PagedResponse<ProductResponse> searchProducts(@RequestParam final String keyword,
+                                                         @RequestParam(required = false) BirthYearRange birthYearRange,
+                                                         @RequestParam(required = false) Gender gender,
                                                          @SortDefault.SortDefaults(value = {
                                                                  @SortDefault(sort = "createdAt", direction = Direction.DESC),
                                                                  @SortDefault(sort = "price")
