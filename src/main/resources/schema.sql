51b4b91432e9b96f8d9f8250ff37b8f1f33b6a50
gunner6603 <gunner6603@snu.ac.kr>
2023-08-26 17:33:02 +0900

    feat: 통계 테이블 스키마 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -81,6 +81,29 @@ CREATE TABLE IF NOT EXISTS rates
     `last_modified_at` datetime              NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS rate_stat
+(
+    `id`               bigint auto_increment NOT NULL primary key,
+    `product_id`       bigint                NOT NULL,
+    `birth_year_range` varchar(255)          NOT NULL,
+    `gender`           varchar(255)          NOT NULL,
+    `total_score`      int                NOT NULL,
+    `count`            int                   NOT NULL,
+    `created_at`       datetime              NOT NULL,
+    `last_modified_at` datetime              NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS order_stat
+(
+    `id`               bigint auto_increment NOT NULL primary key,
+    `product_id`       bigint                NOT NULL,
+    `birth_year_range` varchar(255)          NOT NULL,
+    `gender`           varchar(255)          NOT NULL,
+    `count`            int                   NOT NULL,
+    `created_at`       datetime              NOT NULL,
+    `last_modified_at` datetime              NOT NULL
+);
+
 create index idx_member_orders on orders (member_id);
 create index idx_order_order_items on order_items (order_id);
 create index idx_product_order_items on order_items (product_id);
