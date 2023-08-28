738e7a7bfc7e13cfc2795271133e60c915e55a5e
dongjoo0-0 <kdj486347@gmail.com>
2023-08-28 16:21:36 +0900

    refactor: 별점 높은 순 정렬에 countQuery를 적용한다

--- src/main/java/com/gugucon/shopping/item/repository/ProductRepository.java
+++ src/main/java/com/gugucon/shopping/item/repository/ProductRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -25,12 +24,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "order by sum(oi.quantity.value) desc ")
     Page<Product> findAllByNameSortByOrderCountDesc(@Param("keyword") final String keyword, final Pageable pageable);
 
-    @Query("select p from Product p " +
+    @Query(value = "select p from Product p " +
             "left join OrderItem oi on oi.productId = p.id " +
             "left join Rate r on r.orderItem.id = oi.id " +
             "where p.name like %:keyword% " +
             "group by p.id " +
-            "order by avg(r.score) desc ")
+            "order by avg(r.score) desc ",
+            countQuery = "select count(p.id) from Product p where p.name like %:keyword%")
     Page<Product> findAllByNameSortByRateDesc(@Param("keyword") final String keyword, final Pageable pageable);
 
     @Query("UPDATE Product p " +
