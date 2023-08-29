2d61c4d2cbd10b14ff2006b1469067fec8fd68bf
gunner6603 <gunner6603@snu.ac.kr>
2023-08-29 16:51:50 +0900

    test: 테스트 추가

--- src/test/java/com/gugucon/shopping/order/service/OrderServiceTest.java
+++ src/test/java/com/gugucon/shopping/order/service/OrderServiceTest.java
@@ -1,69 +1,79 @@
 package com.gugucon.shopping.order.service;
 
-import static com.gugucon.shopping.order.domain.entity.Order.OrderStatus.CREATED;
-import static com.gugucon.shopping.utils.DomainUtils.createMember;
-import static com.gugucon.shopping.utils.DomainUtils.createProduct;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-
-import com.gugucon.shopping.common.domain.vo.Quantity;
-import com.gugucon.shopping.item.domain.entity.CartItem;
-import com.gugucon.shopping.item.repository.CartItemRepository;
+import com.gugucon.shopping.common.config.JpaConfig;
+import com.gugucon.shopping.common.exception.ErrorCode;
+import com.gugucon.shopping.common.exception.ShoppingException;
+import com.gugucon.shopping.member.domain.entity.Member;
+import com.gugucon.shopping.member.repository.MemberRepository;
 import com.gugucon.shopping.order.domain.PayType;
 import com.gugucon.shopping.order.domain.entity.Order;
+import com.gugucon.shopping.order.dto.request.OrderPayRequest;
 import com.gugucon.shopping.order.repository.OrderRepository;
-import java.util.List;
+import com.gugucon.shopping.utils.DomainUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
-@ExtendWith(MockitoExtension.class)
-@DisplayName("OrderService 단위 테스트")
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+@Import(value = {OrderService.class, JpaConfig.class})
+@DisplayName("OrderService 통합 테스트")
 class OrderServiceTest {
 
-    @Mock
-    private OrderRepository orderRepository;
-    @Mock
-    private CartItemRepository cartItemRepository;
-    @InjectMocks
+    @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Test
-    @DisplayName("주문한다.")
-    void order() {
-        /* given */
-        final Long memberId = createMember().getId();
-        final CartItem cartItem1 = CartItem.builder()
-                .id(1L)
-                .memberId(memberId)
-                .product(createProduct("치킨", 10000))
-                .quantity(Quantity.from(1))
-                .build();
-        final CartItem cartItem2 = CartItem.builder()
-                .id(2L)
-                .memberId(memberId)
-                .product(createProduct("피자", 20000))
-                .quantity(Quantity.from(2))
-                .build();
-        final List<CartItem> cartItems = List.of(cartItem1, cartItem2);
+    @DisplayName("잘못된 주문 ID로 주문을 조회하면 예외가 발생한다.")
+    void findOrderByFail_invalidOrderId() {
+        // given
+        final Member member = memberRepository.save(DomainUtils.createMember());
 
-        doReturn(cartItems).when(cartItemRepository).findAllByMemberIdWithProduct(memberId);
-        doReturn(Order.builder()
-                         .id(1L)
-                         .memberId(memberId)
-                         .status(CREATED)
-                         .payType(PayType.NONE)
-                         .build())
-                .when(orderRepository).save(any());
+        // when & then
+        final ShoppingException exception = assertThrows(ShoppingException.class,
+                                                         () -> orderService.findOrderBy(1L, member.getId()));
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.INVALID_ORDER);
+    }
 
-        /* when */
-        orderService.order(memberId);
+    @Test
+    @DisplayName("잘못된 주문 ID로 결제를 요청하면 예외가 발생한다.")
+    void requestPayFail_invalidOrderId() {
+        // given
+        final Member member = memberRepository.save(DomainUtils.createMember());
 
-        /* then */
-        verify(orderRepository).save(any());
+        // when & then
+        final ShoppingException exception = assertThrows(ShoppingException.class,
+                                                         () -> orderService.requestPay(
+                                                                 new OrderPayRequest(1L, PayType.POINT),
+                                                                 member.getId()));
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.INVALID_ORDER);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 상품을 주문한 채로 결제를 요청하면 예외가 발생한다.")
+    void requestPayFail_invalidProductId() {
+        // given
+        final Member member = memberRepository.save(DomainUtils.createMember());
+        final Long memberId = member.getId();
+        final Order order = orderRepository.save(Order.from(memberId, List.of(DomainUtils.createCartItem())));
+
+        // when & then
+        final ShoppingException exception = assertThrows(ShoppingException.class,
+                                                         () -> orderService.requestPay(
+                                                                 new OrderPayRequest(order.getId(), PayType.POINT),
+                                                                 memberId));
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.UNKNOWN_ERROR);
     }
 }
