bc22fc7b2090524bc5be81f392eebb228a235234
sen <64001680+dahyen0o@users.noreply.github.com>
2023-08-29 15:16:25 +0900

    chore: 테스트 불가능한 코드를 Jacoco 검사에서 제외 (#143)

--- src/main/java/com/gugucon/shopping/item/repository/OrderStatRepository.java
+++ src/main/java/com/gugucon/shopping/item/repository/OrderStatRepository.java
@@ -1,7 +1,6 @@
 package com.gugucon.shopping.item.repository;
 
 import com.gugucon.shopping.item.domain.entity.OrderStat;
-import com.gugucon.shopping.item.repository.dto.SimpleOrderStatDto;
 import com.gugucon.shopping.member.domain.vo.BirthYearRange;
 import com.gugucon.shopping.member.domain.vo.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,22 +8,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.util.List;
-
 public interface OrderStatRepository extends JpaRepository<OrderStat, Long> {
-
-    @Query("SELECT new com.gugucon.shopping.item.repository.dto.SimpleOrderStatDto(p.id, sum(oi.quantity.value)) " +
-            "FROM Order o " +
-            "JOIN o.orderItems oi " +
-            "JOIN Member m ON o.memberId = m.id " +
-            "JOIN Product p ON p.id = oi.productId " +
-            "WHERE m.birthDate between :startDate AND :endDate " +
-            "AND m.gender = :gender " +
-            "GROUP BY p.id")
-    List<SimpleOrderStatDto> findAllSimpleOrderStatByGenderAndBirthDateBetween(@Param("gender") final Gender gender,
-                                                                               @Param("startDate") final LocalDate startDate,
-                                                                               @Param("endDate") final LocalDate endDate);
 
     @Query("UPDATE OrderStat os " +
             "SET os.count = os.count + :count " +
